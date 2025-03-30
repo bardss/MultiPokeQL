12f93b9cb6cdd6608426d020594de36e4e88c3dd
Jakub Aniola <aniola.j.dev@gmail.com>
2025-03-30 14:35:03 +0200

    Clear unused classes

--- composeApp/src/commonMain/kotlin/com/jakubaniola/multipokeql/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.jakubaniola.multipokeql
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/iosMain/kotlin/com/jakubaniola/multipokeql/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/jakubaniola/multipokeql/MainViewController.kt
@@ -1,5 +1,6 @@
 package com.jakubaniola.multipokeql
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.jakubaniola.multipokeql.home.compose.HomeScreen
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController { HomeScreen() }
