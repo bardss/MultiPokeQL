972147db6714689145911552047b1cc7e126ec27
jakubaniola <aniola.j.dev@gmail.com>
2025-05-08 23:10:47 +0200

    Fix versions to compile on iOS

--- composeApp/src/commonMain/kotlin/com/jakubaniola/multipokeql/ui/home/compose/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/jakubaniola/multipokeql/ui/home/compose/HomeScreen.kt
@@ -2,7 +2,6 @@ package com.jakubaniola.multipokeql.ui.home.compose
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
